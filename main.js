04a7e269e95e0356e43e691a8e788120e2c86478
Iara Martinelli <iaramarti789@gmail.com>
2023-02-28 17:02:17 -0300

    Quinta versión 28/02

---
+++ main.js
@@ -0,0 +1,34 @@
+function ingresarMouse1(event) {
+    document.getElementById('proy1').style.background = '#fef08a';
+    document.getElementById('p1').style.color = 'black';
+    document.getElementById('num1').style.color = 'black';
+}
+
+function ingresarMouse2(event) {
+    document.getElementById( 'proy2').style.background = '#fef08a';
+    document.getElementById('p2').style.color = 'black';
+    document.getElementById('num2').style.color = 'black';
+}
+function ingresarMouse3(event) {
+    document.getElementById('proy3').style.background = '#fef08a';
+    document.getElementById('p3').style.color = 'black';
+    document.getElementById('num3').style.color = 'black';
+}
+
+function sacarMouse1(event) {
+    document.getElementById('proy1').style.background = '#9ca3af';
+    document.getElementById('p1').style.color = 'white';
+    document.getElementById('num1').style.color = 'white';
+}
+
+function sacarMouse2(event) {
+    document.getElementById('proy2').style.background = '#9ca3af';
+    document.getElementById('p2').style.color = 'white';
+    document.getElementById('num2').style.color = 'white';
+}
+
+function sacarMouse3(event) {
+    document.getElementById('proy3').style.background = '#9ca3af';
+    document.getElementById('p3').style.color = 'white';
+    document.getElementById('num3').style.color = 'white';
+}
