49c200753ce23bfbb7b595d7d848439a18e6d5b4
Iara Martinelli <iaramarti789@gmail.com>
2023-03-01 16:08:58 -0300

    Versión final 01/03

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function ingresarMouse1(event) {
-    document.getElementById('proy1').style.background = '#fef08a';
+    document.getElementById('proy1').style.background = '#dcfce7';
     document.getElementById('p1').style.color = 'black';
     document.getElementById('num1').style.color = 'black';
 }
 
 function ingresarMouse2(event) {
-    document.getElementById( 'proy2').style.background = '#fef08a';
+    document.getElementById( 'proy2').style.background = '#dcfce7';
     document.getElementById('p2').style.color = 'black';
     document.getElementById('num2').style.color = 'black';
 }
 function ingresarMouse3(event) {
-    document.getElementById('proy3').style.background = '#fef08a';
+    document.getElementById('proy3').style.background = '#dcfce7';
     document.getElementById('p3').style.color = 'black';
     document.getElementById('num3').style.color = 'black';
 }
